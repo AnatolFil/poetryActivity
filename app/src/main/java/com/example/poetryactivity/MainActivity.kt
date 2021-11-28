4a492f962ef9260fc3e1b76dfa7ad980df121db5
AnatolFil <tolyanfilatov@mail.ru>
2021-11-28 12:05:47 +0300

    finished poetry app

--- app/src/main/java/com/example/poetryactivity/MainActivity.kt
+++ app/src/main/java/com/example/poetryactivity/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.poetryactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    private var TAG: String = "Poetry"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "Ты видел деву на скале")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "В одежде белой над волнами")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "Когда, бушуя в бурной мгле,")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "Играло море с берегами,")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "Когда луч молний озарял")
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "Ее всечасно блеском алым")
+        super.onDestroy()
     }
 }
